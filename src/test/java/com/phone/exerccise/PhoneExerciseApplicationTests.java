ca89c0cbd9461e9566440d8a28756a28ed99594c
Ankur Bansal <ankur.ete@gmail.com>
2020-04-15 00:59:15 +0100

    First commit

---
+++ src/test/java/com/phone/exerccise/PhoneExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phone.exerccise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
